ed9808e23a5b9df83da824d4cec795dbb5502493
Olga Dobrova <dobrova_o_g@mail.ru>
2023-08-19 15:31:16 +0300

    произведение цифр числа

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const title = "Это усложненные задания";
+const title = "усложненные задания. Урок 2.";
+let num = 266219;
+let a = 1; //нач значение произведения цифр num
+
+num = String(num);
+for (let i = 0; i <= num.length - 1; i++) {
+  a *= +num[i];
+}
+console.log("произведение цифр числа " + num + " = " + a);
