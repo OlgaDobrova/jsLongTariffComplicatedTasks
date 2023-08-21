70aa5095a459714674cd2815c3f42ec81218b00e
Olga Dobrova <dobrova_o_g@mail.ru>
2023-08-21 23:01:19 +0300

    усложн зад к уроку №3

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 const title = "Это усложненные задания";
+let lang = "ru"; //lang может принимать только 2 значения!: 'ru' 'en'
+let langArray = [
+  ["ru", "пн, вт, ср, чт, пт, сб, вс"],
+  ["en", "mo, tu, we, th, fr, sa, su"],
+];
+let namePerson = "Александр";
+console.log(" lang = " + lang);
+if (lang === "ru") {
+  console.log("пн, вт, ср, чт, пт, сб, вс");
+} else {
+  console.log("mo, tu, we, th, fr, sa, su");
+}
+switch (true) {
+  case lang == "ru":
+    console.log("пн, вт, ср, чт, пт, сб, вс");
+    break;
+  case lang == "en":
+    console.log("mo, tu, we, th, fr, sa, su");
+    break;
+  default:
+    console.log("нет такого! всего 2 варианта!");
+}
+
+lang == langArray[0][0]
+  ? console.log(langArray[0][1])
+  : console.log(langArray[1][1]);
+namePerson == "Артем"
+  ? console.log("директор")
+  : namePerson == "Александр"
+  ? console.log("преподаватель")
+  : console.log("студент");
