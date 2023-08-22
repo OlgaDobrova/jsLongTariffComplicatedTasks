c9aa7903cb46ff23d9e74516e1f39410547d4d56
Olga Dobrova <dobrova_o_g@mail.ru>
2023-08-22 22:10:42 +0300

    добавила комментарии

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ let langArray = [
 ];
 let namePerson = "Александр";
 console.log(" lang = " + lang);
+
+//a) через if,
 if (lang === "ru") {
   console.log("пн, вт, ср, чт, пт, сб, вс");
 } else {
   console.log("mo, tu, we, th, fr, sa, su");
 }
+//b) через switch-case
 switch (true) {
   case lang == "ru":
     console.log("пн, вт, ср, чт, пт, сб, вс");
@@ -21,7 +24,7 @@ switch (true) {
   default:
     console.log("нет такого! всего 2 варианта!");
 }
-
+//c) через многомерный массив без ифов и switch.
 lang == langArray[0][0]
   ? console.log(langArray[0][1])
   : console.log(langArray[1][1]);
