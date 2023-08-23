265b2b2f6657bdbf1768dbfdf8d5f937d107bc72
Olga Dobrova <dobrova_o_g@mail.ru>
2023-08-23 20:57:28 +0300

    вывод через объект

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+"use strict";
 const title = "Это усложненные задания";
+
 const lang = "ru"; //lang может принимать только 2 значения!: 'ru' 'en'
-const langArray = [
-  ["ru", "пн, вт, ср, чт, пт, сб, вс"],
-  ["en", "mo, tu, we, th, fr, sa, su"],
-];
+const langObject = {
+  ru: ["пн, вт, ср, чт, пт, сб, вс"],
+  en: ["mo, tu, we, th, fr, sa, su"],
+};
 const namePerson = "Александр";
+
 console.log(" lang = " + lang);
 
 //a) через if,
@@ -13,6 +16,7 @@ if (lang === "ru") {
 } else {
   console.log("mo, tu, we, th, fr, sa, su");
 }
+
 //b) через switch-case
 switch (true) {
   case lang == "ru":
@@ -24,8 +28,10 @@ switch (true) {
   default:
     console.log("нет такого! всего 2 варианта!");
 }
+
 //c) через многомерный массив без ифов и switch.
-console.log(langArray[1][1]);
+console.log(langObject[lang]);
+
 //задача про должности с помощью нескольких тернарных операторов
 namePerson == "Артем"
   ? console.log("директор")
