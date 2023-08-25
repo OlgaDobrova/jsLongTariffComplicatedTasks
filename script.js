8119635111709b64516f7c58c292b6aa50df0d98
Olga Dobrova <dobrova_o_g@mail.ru>
2023-08-25 19:35:23 +0300

    выполнено. Усл задание 4

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-const title = "Это усложненные задания";
+"use strict";
+//Это Урок №4 усложненные задания
+
+const transformString = function (variable) {
+  //если передана строка, то
+  //удаление пробелов в начале и в конце строки
+  //обрезка строки до 30 символов, потом ...
+  variable =
+    typeof variable !== "string"
+      ? "передана переменная типа " + typeof variable
+      : variable.trim().length >= 30
+      ? variable.trim().substring(0, 30) + "..."
+      : variable.trim().substring(0, variable.trim().length) + "...";
+  return variable;
+};
+
+console.log(transformString(123));
+console.log(transformString(7 + 5));
+console.log(transformString(7 > 5));
+console.log(transformString("Карл у Клары украл кораллы"));
+console.log(
+  transformString("Карл у Клары украл кораллы, Клара у Карла украла кларнет")
+);
