831ff1a738d4a64409caeb24aeca765a167bafcf
Olga Dobrova <dobrova_o_g@mail.ru>
2023-08-26 13:15:50 +0300

    урок 5. усл зад 1

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-const title = "Это усложненные задания";
+"use strict";
+//Это Урок №5 усложненное задание №1
+
+const showTypeOff = function (variable) {
+  return variable + " " + typeof variable;
+};
+//вывод в переменную числа, после проверки: число ли это?
+const isNumber = function (variable) {
+  // проверка: это число? -> true/false
+  if (!isNaN(parseFloat(variable)) && isFinite(variable)) {
+    variable = parseFloat(variable);
+  } else {
+    variable = 0;
+  }
+  return variable;
+};
+
+console.log(showTypeOff(isNumber(123)));
+console.log(showTypeOff(isNumber("   123   ")));
+console.log(showTypeOff(isNumber(0)));
+console.log(showTypeOff(isNumber(NaN)));
+console.log(showTypeOff(isNumber("   123asdddd")));
